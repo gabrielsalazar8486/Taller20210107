ac9716536215dbca0b79e6740682b68ef4243f03
Daniel Marcano <dmarcanor2@gmail.com>
2021-01-09 17:04:42 -0400

    Feature(): Assistant controller and javascript event

---
+++ Daniel/app/AssistantController/AssistantGetController.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AssistantController\AsisstantController;
+
+final class AssistantGetController
+{
+    public function get(): void
+    {
+        exit("hello world");
+    }
+}
