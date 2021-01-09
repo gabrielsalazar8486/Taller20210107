b0d6d1682d167d73aef87f385268100cd8355c3d
Jose Quilarque <josequiloz@gmail.com>
2021-01-09 15:28:00 -0400

    Almost Done

--- Jose_Quilarque/src/inverter.js
+++ Jose_Quilarque/src/inverter.js
@@ -1,8 +1,25 @@
 new Vue({
-    el: 'inverter',
+    el: '#inverter',
     data(){
         return{
-            title: 'Title'
+            textedInput: '',
+            invertedText:'',
+            reInvertedText:''
+        }
+    },
+    methods:{
+        invertWords(){
+            let textToInvert = this.textedInput;
+            let invertText = '';
+            for (let i = textToInvert.length - 1; i >= 0; i--) {
+                invertText += textToInvert[i];
+            }
+            this.invertedText = invertText;
+        },
+
+        erase(){
+            this.textedInput = '';
+            this.invertedText = '';
         }
     }
 })
