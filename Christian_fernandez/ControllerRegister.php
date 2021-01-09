c6d4e3a46e817f46e7b2e75d58c683a4381a80d4
Chmalo <chmalo.f@gmail.com>
2021-01-09 14:59:45 -0400

    TallerChristian

---
+++ Christian_fernandez/ControllerRegister.php
@@ -0,0 +1,39 @@
+<?php
+
+
+final class ControllerRegister
+{
+    private $file;
+    private $datos;
+
+    public function __construct()
+    {
+        $this->datos = $_POST;
+        $this->file = './directorio';
+    }
+
+    public function getDirectorio(): array
+    {
+
+        $json_directory = $this->getDataInFile();
+        $result = $this->separar_registros($json_directory);
+
+        return $result;
+
+    }
+
+    protected function getDataInFile(): string
+    {
+        $file = $this->file;
+        $directorio = fopen($file, 'r+');
+        $result = fread($directorio, filesize($file));
+        fclose($directorio);
+        return $result;
+    }
+
+    protected function separar_registros(string $json_directory): array
+    {
+        $result = !empty($json_directory) ? explode('--', $json_directory) : [];
+        return array_filter($result);
+    }
+}
